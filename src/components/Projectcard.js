798aa187ee6007f258b9a35bd9a23fd6ae041367
Rachel Murray <dcbeergoddess@gmail.com>
2022-02-20 18:54:28 -0500

    spelling error

--- src/components/Projectcard.js
+++ src/components/Projectcard.js
@@ -2,31 +2,49 @@ import React, { Component } from 'react';
 import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class Projectcard extends Component {
-
   render() {
-
     return (
-
       <>
-        <Card style={{ width: '18rem', display:"flex"}}>
-          <Card.Img className="img-style" variant="top" alt={this.props.title} src={this.props.image} />
+        <Card style={{ width: '18rem', display: 'flex' }}>
+          <Card.Img
+            className='img-style'
+            variant='top'
+            alt={this.props.title}
+            src={this.props.image}
+          />
           <Card.Body>
-            <Card.Title className="title">{this.props.title}</Card.Title>
-          </Card.Body>  
-            <ListGroup className="list-group-flush">
-              <ListGroupItem style={{background:"ivory"}}><strong>Description:</strong> {this.props.description}</ListGroupItem>
-              <ListGroupItem style={{background:"ivory"}}><strong>Tech Stack:</strong> {this.props.techstack}</ListGroupItem>
-            </ListGroup>
-          <Card.Body> 
-            <Button variant="outline-dark" href={this.props.github} target="_blank" rel="noopener noreferrer">Github Rep</Button>
-            <Button variant="outline-dark" href={this.props.deploy} target="_blank" rel="noopener noreferrer">Deployed Project</Button>
-            </Card.Body>   
-          
+            <Card.Title className='title'>{this.props.title}</Card.Title>
+          </Card.Body>
+          <ListGroup className='list-group-flush'>
+            <ListGroupItem style={{ background: 'ivory' }}>
+              <strong>Description:</strong> {this.props.description}
+            </ListGroupItem>
+            <ListGroupItem style={{ background: 'ivory' }}>
+              <strong>Tech Stack:</strong> {this.props.techstack}
+            </ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Button
+              variant='outline-dark'
+              href={this.props.github}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Github Repo
+            </Button>
+            <Button
+              variant='outline-dark'
+              href={this.props.deploy}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Deployed Project
+            </Button>
+          </Card.Body>
         </Card>
-      </>  
-      
-    )
+      </>
+    );
   }
 }
 
-export default Projectcard
+export default Projectcard;
